eddc97d8fb0e1188d292cab148f27b18e0a2d6ae
isaach0011 <ihil17523@740s-pl-177.local>
2016-11-10 12:41:06 -0700

    Created arrays and made constructor and model.

--- src/array/controller/ArrayController.java
+++ src/array/controller/ArrayController.java
@@ -1,9 +1,45 @@
 package array.controller;
 
+import array.model.Hipster;
+//import array.view.HipsterFrame;
+
 public class ArrayController
 {
+	//private HipsterFrame appFrame;
+	private int [] numbers;
+	private String [] words = {"This ","is the ","second hipster","level: ","initialization ","lists"};
+	private Hipster [] hipsters;
+	
+	
+	public ArrayController()
+	{
+		numbers = new int [20];
+
+		hipsters = new Hipster [6];
+	}
+	
 	public void start()
 	{
 		
+		for(int index = 0; index < hipsters.length; index++)
+		{
+			hipsters[index] = new Hipster("Hipster # " + index);
+		}
+		
+		
+		for(Hipster currentHipster : hipsters)
+		{
+			System.out.print(currentHipster + ", ");
+		}
+		System.out.println("Next");
+		for(String currentWords : words)
+		{
+			System.out.print(currentWords + ", ");
+		}
+		System.out.println("Next");
+		for(int tempInt : numbers)
+		{
+			System.out.print(tempInt + ", ");
+		}
 	}
 }
---
+++ src/array/model/Hipster.java
@@ -0,0 +1,19 @@
+package array.model;
+
+public class Hipster
+{
+	
+	private String name;
+	
+	public Hipster(String name)
+	{
+		this.name = name;
+	}
+	
+	public String toString()
+	{
+		String description = "My name is " + name;
+		
+		return description;
+	}
+}
